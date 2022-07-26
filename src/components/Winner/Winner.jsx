2baafe8640cb2489f44ab6b70e94d9ffc8b3c075
Ronin <compyneller@gmail.com>
2022-07-26 15:17:33 +0530

    add content

--- src/components/Winner/Winner.jsx
+++ src/components/Winner/Winner.jsx
@@ -1,29 +1,24 @@
 import React from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import style from "./Winner.module.scss";
+import image from "../../assets/6037680.png";
 const Winner = () => {
   return (
     <Container style={{ padding: "3rem 0" }}>
-      <Row className="g-3">
-        <Col
-          sm={12}
-          lg={6}
-          className="d-flex align-items-start flex-column justify-content-center"
-          id={style.content}
-        >
-          <h1>Winner</h1>
-          <p className="text-secondary">
-            You start with the default level of bronze with 0 points and these
-            levels go all the way up to Platinum with 5000 coins
-          </p>
+      <Row classNam="g-3">
+        <Col sm={12} lg={6} className={style.content}>
+          <h1>PLAYPENNY- YOUR FAVORITE RUMMY SITE</h1>
+          <h5>
+            Playpenny offers the 13 cards classic Rummy game with an exciting
+            bonus and gets the cash prizes from the best online rummy site.
+            Whether you are bored in your home, tired from your boss or
+            traveling in outstation, Playpenny is always with you! Register,
+            Login, Play free rummy as well as rummy for cash and win huge cash
+            prizes.
+          </h5>
         </Col>
         <Col sm={12} lg={6}>
-          <img
-            src="https://images.unsplash.com/photo-1634313945702-56a8073970bf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=986&q=80"
-            className="w-100"
-            alt=""
-            style={{ borderRadius: "10px" }}
-          />
+          <img src={image} className="w-100" alt="" />
         </Col>
       </Row>
     </Container>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { Col, Container, Row } from "react-bootstrap";
-import AccordionComp from "../components/AccordionComp";
+import Bricks from "../components/Bricks/Bricks";
 import CardComp from "../components/Cards/CardComp";
 import HeaderComp from "../components/HeaderComp/HeaderComp";
+import Rewards from "../components/Rewards/Rewards";
 import TopMostComp from "../components/TopMostComp/TopMostComp";
-import WhatWinner from "../components/WhatWinnerSays/WhatWinner";
 import Winner from "../components/Winner/Winner";
 
 const Home = () => {
@@ -13,17 +13,9 @@ const Home = () => {
       <HeaderComp />
       <TopMostComp />
       <CardComp />
-      <WhatWinner />
-      <Container style={{ padding: "3rem 0" }}>
-        <Row className="g-3">
-          <Col sm={12} lg={6}>
-            <AccordionComp />
-          </Col>
-          <Col sm={12} lg={6}>
-            <AccordionComp />
-          </Col>
-        </Row>
-      </Container>
+      <Winner />
+      <Bricks />
+      <Rewards />
     </>
   );
 };
