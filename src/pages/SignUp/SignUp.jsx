298cc4b6699f1939f9718eaed80ea2d780dd3111
Ronin <compyneller@gmail.com>
2022-08-04 12:19:55 +0530

    remove insecure

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -30,7 +30,7 @@ const SignUp = () => {
     };
     console.log(body);
     const response = await axios.post(
-      "https://34.207.41.229:4100/playpenny/signup",
+      "http://34.207.41.229:4100/playpenny/signup",
       body,
       {
         headers: {
